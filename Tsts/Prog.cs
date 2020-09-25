59b655a30961d9914a3cf13a5a6233199f9a5996
Ivan <ivanovvaha2004@gmail.com>
2020-09-25 23:07:09 +0300

    first commit

---
+++ Tsts/Prog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Tsts
+{
+    public class Prog
+    {
+        private double smth;
+        public Prog(double price)
+        {
+            this.smth = price;
+        }
+        
+        public double GetPrice()
+        {
+            return smth;
+        }
+        public void IncreasePrice(double count)
+            => smth *= count;
+
+        public void AddPrice(double count)
+            => smth += count;
+        
+        public void SharePrice(double count)
+            => smth /= count;
+
+        public void SubtractNumFromPrice(double count)
+            => smth -= count;
+    }
+}
